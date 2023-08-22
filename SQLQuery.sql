0a1e392cadfdc001b89f5b4ec9838ab41bba6d5e
MarshiMarshi <gustavogr1806@gmail.com>
2023-08-22 11:02:28 -0300

    Criação tabela Usuario

---
+++ SQLQuery.sql
@@ -0,0 +1,2 @@
+create database mind
+use mind
--- src/main/java/br/com/etechoracio/mind/entity/Usuario.java
+++ src/main/java/br/com/etechoracio/mind/entity/Usuario.java
@@ -1,5 +1,37 @@
 package br.com.etechoracio.mind.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@Entity
+@Table(name="")
 public class Usuario {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="")
+    private int id;
+
+    @Column(name="")
+    private String nome;
+
+    @Column(name="")
+    private String sobrenome;
+
+    @Column(name="")
+    private String email;
+
+    @Column(name="")
+    private String senha;
+
+    @Column(name="")
+    private LocalDate dataNascimento;
+
+    @Column(name="")
+    private int totalCoins;
 }
